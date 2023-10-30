2ac18c5ee8fe780b267ce8bb703d33037c120dcf
PedroMalagutti03 <113479247+PedroMalagutti03@users.noreply.github.com>
2023-10-30 19:04:29 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// script.js
+
+// Função para alternar entre os temas claro e escuro
+function toggleTheme() {
+    const body = document.body;
+    console.log("Botão de alternância de tema clicado!"); // Verifique no console do navegador
+    body.classList.toggle("dark-mode");
+}
